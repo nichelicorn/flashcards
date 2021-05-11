821e5c8b6f6b376607616dff2ca243524a8176c0
Nichele Dunn <the7yearitch@hotmail.com>
2021-05-11 13:57:27 -0600

    ✨ Add tests for constructor to Turn-test

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -4,15 +4,42 @@ const expect = chai.expect;
 const Turn = require('../src/Turn');
 
 describe('Turn class', () => {
-  let turn1, turn2;
+  let card1, card2, card3, turn1, turn2, turn3;
   beforeEach(() => {
-    turn1 = new Turn();
-    turn2 = new Turn();
+    card1 = {
+      "id": 1,
+      "question": "At what age did Ahsoka Tano become a Jedi Padawan?",
+      "answers": ["12", "14", "17"],
+      "correctAnswer": "14"
+    };
+    card2 = {
+      "id": 2,
+      "question": "Who was Ahsoka's teacher?",
+      "answers": ["Yoda", "Plo Koon", "Anakin Skywalker"],
+      "correctAnswer": "Anakin Skywalker"
+    };
+    card3 = {
+      "id": 3,
+      "question": "Did Ahsoka fight with Darth Maul on Mandalore?",
+      "answers": ["true", "false"],
+      "correctAnswer": "true"
+    };
+    turn1 = new Turn('14', card1);
+    turn2 = new Turn('Yoda', card2);
+    turn3 = new Turn('true', card3);
   })
 
   describe('Turn class properties', () => {
     it('should store a user guess', () => {
-      expect()
+      expect(turn1.userGuess).to.equal('14');
+      expect(turn2.userGuess).to.equal('Yoda');
+      expect(turn3.userGuess).to.equal('true');
+    })
+
+    it('should store a card in play', () => {
+      expect(turn1.currentCard).to.deep.equal(card1); // test that it takes in an object
+      expect(turn2.currentCard.id).to.equal(2); // test that the object can be read using dot notation
+      expect(turn3.currentCard[correctAnswer]).to.equal('true'); // test that the object can be read using bracket notation
     })
   })
 
@@ -21,7 +48,7 @@ describe('Turn class', () => {
 })
 
 // Your Turn class should meet the following requirements:
-// Instantiated with two arguments - a string (that represents a user’s guess to the question), and a Card object for the current card in play.
+// Instantiated with two arguments - √ a string (that represents a user’s guess to the question), and a Card object for the current card in play.
 // returnGuess: method that returns the guess
 // returnCard: method that returns the Card
 // evaluateGuess: method that returns a boolean indicating if the user’s guess matches the correct answer on the card
