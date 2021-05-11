111ad421935afcd90d4a8cdef20862f9884799a7
Nichele Dunn <the7yearitch@hotmail.com>
2021-05-11 16:46:07 -0600

    ✨ Add require statements to Deck-test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,3 +1,9 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck');
+
+
 
 // constructor
 // Your Deck class should be initialized with an array of Card objects and
