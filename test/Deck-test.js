9b38b9dfef39db089268e6bf4ed7ff2222da29c7
Nichele Dunn <the7yearitch@hotmail.com>
2021-05-11 16:44:31 -0600

    📝 Add pseudo to deck-test

---
+++ test/Deck-test.js
@@ -0,0 +1,6 @@
+
+// constructor
+// Your Deck class should be initialized with an array of Card objects and
+
+// method Deck.countCards
+// should have an accompanying test file. It should know how many Cards are in the Deck.
