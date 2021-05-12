09f47af3196a3dabda40ca86a216d26fddcfcd08
Nichele Dunn <the7yearitch@hotmail.com>
2021-05-11 19:04:56 -0600

    ♻️  Delete pseudo

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -36,9 +36,3 @@ describe('Deck class', () => {
     expect(deck.countCards()).to.equal(3);
   })
 })
-
-// constructor
-// Your Deck class should be initialized with an array of Card objects and
-
-// method Deck.countCards
-// should have an accompanying test file. It should know how many Cards are in the Deck.
