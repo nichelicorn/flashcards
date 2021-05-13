d0239899546a6174a442cab7ce92d6f8937f8ff1
Nichele Dunn <the7yearitch@hotmail.com>
2021-05-12 19:50:24 -0600

    ✨ Add require statements to Game-test

---
+++ test/Game-test.js
@@ -0,0 +1,8 @@
+const chai = require('Chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Game = require('../src/Game');
+const Round = require('../src/Round');
+const Turn = require('../src/Turn');
