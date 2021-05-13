6f6229221523180156a5dc577e12feae996d6010
Nichele Dunn <the7yearitch@hotmail.com>
2021-05-13 07:33:26 -0600

    ♻️  Delete pseudo

--- src/Game.js
+++ src/Game.js
@@ -11,22 +11,12 @@ class Game {
   }
 
   start() {
-    // console.log('prototypeQuestions', prototypeQuestions); // an array of objects
-    // iterate through the array prototypeQuestions and extract each object
-    // instantiate a new Card with each object from the prototypeQuestions array
-    // instantiate new Deck using card array
     const cards = prototypeQuestions.map(question => {
-      // console.log('question', question);
       const card = new Card(question.id, question.question, question.answers, question.correctAnswer);
-      // console.log(card);
       return card;
     });
     const deck = new Deck(cards);
-    // console.log(deck);
     this.currentRound = new Round(deck);
-    // instantiate new Round using the new Deck
-    // console.log('currentRound', this.currentRound)
-
   }
 
   printMessage(deck, round) {
