f1458251aa040bae7714d380122aa32f1125d681
Nichele Dunn <the7yearitch@hotmail.com>
2021-05-12 20:51:10 -0600

    ♻️  Refactor Game-test

--- test/Game-test.js
+++ test/Game-test.js
@@ -38,7 +38,7 @@ describe('Game class', () => {
     expect(game).to.be.an.instanceof(Game);
   })
 
-  it('should keep track of the current Round', () => {
+  it('should store the current Round', () => {
     expect(game.currentRound).to.deep.equal({});
   })
 
@@ -47,17 +47,24 @@ describe('Game class', () => {
       expect(game.start).to.be.a('function');
     })
 
-    it('should keep track of the current round', () => {
-      game.start();
-
-      expect(game.currentRound).to.be.an('object');
-    })
-
     it.skip('should create Cards for the deck', () => {
+      game.start()
+
+      expect(game.currentRound.deckInPlay[0]).to.be.an.instanceof(Card);
+    })
+
+    it.skip('should put Cards in the Deck', () => {
       game.start();
 
-      expect(game.currentRound.deckInPlay).to.deep.equal(deck);
+      expect(game.currentRound.deckInPlay).to.be.an.instanceof(Deck);
     })
+
+    it.skip('should start a new Round', () => {
+      game.start();
+
+      expect(game.currentRound).to.be.an.instanceof(Round);
+    })
+
 
   })
 
