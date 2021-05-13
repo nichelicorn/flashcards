7eaf7a804ced6fcd46d6adb22f8ce9ae3f5d9b32
Nichele Dunn <the7yearitch@hotmail.com>
2021-05-12 19:52:37 -0600

    ✨ Add hook to Game-test

--- test/Game-test.js
+++ test/Game-test.js
@@ -6,3 +6,36 @@ const Deck = require('../src/Deck');
 const Game = require('../src/Game');
 const Round = require('../src/Round');
 const Turn = require('../src/Turn');
+
+describe('Game class', () => {
+  let card1, card2, card3, deck, round, game, turn1, turn2, turn3;
+
+  beforeEach(() => {
+    card1 = {
+      "id": 1,
+      "question": "At what age did Ahsoka Tano become a Jedi Padawan?",
+      "answers": ["12", "14", "17"],
+      "correctAnswer": "14"
+    };
+    card2 = {
+      "id": 2,
+      "question": "Who was Ahsoka's teacher?",
+      "answers": ["Yoda", "Plo Koon", "Anakin Skywalker"],
+      "correctAnswer": "Anakin Skywalker"
+    };
+    card3 = {
+      "id": 3,
+      "question": "Did Ahsoka fight with Darth Maul on Mandalore?",
+      "answers": ["true", "false"],
+      "correctAnswer": "true"
+    };
+    deck = new Deck([card1, card2, card3]);
+    turn1 = new Turn('14', card1);
+    turn2 = new Turn('Yoda', card2);
+    turn3 = new Turn('true', card3);
+    round = new Round(deck);
+    game = new Game();
+  })
+
+
+})
